7c4fbea8aa7c1864b81fd25563fc193a1db92945
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-06-01 12:28:38 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000; // стоимость билета
+        int mile = 20; // количество рублей за милю
+        int bonus = ticket / mile ;// получено бонусов за билет
+        System.out.println((bonus + " Бонусных миль"));
+
+    }
+}
