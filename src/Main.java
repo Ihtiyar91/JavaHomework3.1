3d26665f9c2fcce7f5ffbec92301ae2b74b327db
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-06-01 14:31:53 +0300

    mod commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 5000; // стоимость билета
-        int mile = 20; // количество рублей за милю
-        int bonus = ticket / mile ;// получено бонусов за билет
-        System.out.println((bonus + " Бонусных миль"));
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
